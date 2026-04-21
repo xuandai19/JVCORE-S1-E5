8e08602b5a0bc24f011a08ba9a438c4c4e811fac
Xuân Đại <daik5xuanvinh@gmail.com>
2026-04-21 22:27:38 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        final float PI = 3.14f;
 
-        System.out.print("bán kính : ");
-        float k1 = sc.nextFloat();
-        System.out.printf("Diện tích : %.2f", PI * k1 * k1);
+        System.out.print("Cân nặng = ");
+        float cn = sc.nextInt();
+        System.out.print("Chiều cao = ");
+        float ch = sc.nextFloat();
+
+        System.out.printf("Chỉ số BMI = %.2f", cn / (ch * ch));
     }
 }
